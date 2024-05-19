feb180db818a11bb2393c5fcdb97bffdc8bb1649
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2024-05-19 22:14:45 +0700

    feat : Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { HiOutlineHome } from "react-icons/hi";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hello Naw</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <HiOutlineHome /> }]}
+        />
+        <Hero />
       </div>
     </main>
   );
